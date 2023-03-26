cbd006d3b06077b5a866e2024dec3be36de5870a
atakee72 <atakee@gmail.com>
2023-03-26 12:36:25 +0200

    Modified the app.js file with import statements instead of require..

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const schema = require("./schema/schema");
-const mongoose = require("mongoose");
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema/schema";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const mongoKey = process.env.MONGODB_KEY;
 
 mongoose.connect(
-  "mongodb+srv://atakee:fGjERYZ3xPPwm4G@cluster0.bg6julm.mongodb.net/test"
+  `mongodb+srv://atakee:${mongoKey}@cluster0.bg6julm.mongodb.net/test`
 );
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
